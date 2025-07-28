ab20e5164e6823ef733b1855800e953b3688676a
mnshireesha1 <mnshireesha@gmail.com>
2025-07-28 18:05:55 +0530

    operators

--- 02_operators.py
+++ 02_operators.py
@@ -7,3 +7,14 @@ print(a / b)   # 3.33
 print(a // b)  # 3 (floor division)
 print(a % b)   # 1 (modulus)
 print(a ** b)  # 1000 (exponentiation)
+
+#comparison operators
+print(a > b)   # True
+print(a == b)  # False
+
+#Logical opearators
+x = True
+y = False
+print(x and y)  # False
+print(x or y)   # True
+print(not x)    # False
